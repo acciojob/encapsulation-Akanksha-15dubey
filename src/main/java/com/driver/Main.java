01e3a68d7c4b27a127cbdaef9766d4fc8a87c4ff
dubey <dubeyakanksha031@gmail.com>
2023-09-13 16:30:52 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        // This will result in a compilation error
+        obj.name = "John"; // Error: name has private access in RWOnly
+
+        // Access the value using the getter
+        String name = obj.getName();
+        System.out.println("Name: " + name);
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,20 @@
 package com.driver;
 
 public class RWOnly {
-    
+     String name;
+
+    // Constructor
+    public RWOnly() {
+        this.name = ""; // Initialize name with an empty string
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
 }
